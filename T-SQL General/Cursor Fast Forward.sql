cc4a37a6989664c106c0872c8261e1ce521ce88e
Greg Tasev <grigor.tasev@gmail.com>
2017-08-18 11:01:55 -0400

    Initial Version

---
+++ T-SQL General/Cursor Fast Forward.sql
@@ -0,0 +1,19 @@
+DECLARE @Colm NVARCHAR(255)
+
+DECLARE MyCursor01 CURSOR LOCAL FAST_FORWARD FOR
+SELECT name FROM msdb.dbo.syscolumns
+
+OPEN MyCursor01 WHILE 1 = 1
+BEGIN FETCH MyCursor01 INTO
+  @Colm
+
+  IF @@FETCH_STATUS <> 0 BREAK
+
+  PRINT(@Colm)
+  
+END -- MyCursor01
+CLOSE MyCursor01
+DEALLOCATE MyCursor01
+
+
+
